38b408709ec9917d60cea7e24bf0eb96548df564
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-02 07:35:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -584,7 +584,7 @@ async def start(client, message):
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
-                InlineKeyboardButton("♻️ ᴠᴇʀɪғʏ 🎭"", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+                InlineKeyboardButton("♻️ ᴠᴇʀɪғʏ 🎭", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
             ],[
                 InlineKeyboardButton("ʜᴏᴡ ᴛᴏ ᴠᴇʀɪғʏ ❓", url=VERIFY_TUTORIAL)
             ]]
