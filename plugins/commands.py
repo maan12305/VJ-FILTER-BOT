723ac17aa424049a710e6ba0fea3605e4e0c2e5f
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-10-06 13:03:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,7 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from pm_filter import auto_filter
+from .pm_filter import auto_filter
 import os, string, logging, random, asyncio, time, datetime, re, sys, json, base64
 from Script import script
 from pyrogram import Client, filters, enums
