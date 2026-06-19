cf083c7501438f7e0e0155748bd211cdc277956a
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-19 21:19:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2829,10 +2829,16 @@ async def advantage_spell_chok(client, name, msg, reply_msg, vj_search):
         ]]
         if NO_RESULTS_MSG:
             await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
-        k = await reply_msg.edit_text(text=script.I_CUDNT.format(mv_rqst), reply_markup=InlineKeyboardMarkup(button))
-        await asyncio.sleep(30)
-        await k.delete()
-        return
+        new_text = script.I_CUDNT.format(mv_rqst)
+
+    if reply_msg.text != new_text:
+    k = await reply_msg.edit_text(
+        text=new_text,
+        reply_markup=InlineKeyboardMarkup(button)
+    )        
+    await asyncio.sleep(30)        
+    await k.delete()
+    return    
     movielist += [movie.get('title') for movie in movies]
     movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
     SPELL_CHECK[mv_id] = movielist
