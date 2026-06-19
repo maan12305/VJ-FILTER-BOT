4d90256134fd633582a34b338abefeb60717fc2c
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-19 10:07:51 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,7 +1,7 @@
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 from info import CHANNELS
 from database.ia_filterdb import save_file
@@ -13,4 +13,16 @@ async def media(bot, message):
     media = getattr(message, message.media.value, None)
     media.caption = message.caption
     await save_file(media)
+    caption = message.caption or "New Movie Uploaded ✅"
+
+buttons = InlineKeyboardMarkup(
+    [[InlineKeyboardButton("📂 Get File", url="https://t.me/maan_file_store_BOT?start=start")]]
+)
+
+await bot.send_message(
+    chat_id=-1003730484035,
+    text=caption,
+    reply_markup=buttons,
+    disable_web_page_preview=True
+)
 
