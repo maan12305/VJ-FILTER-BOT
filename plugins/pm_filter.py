e2ab7c1ad3e8c414b454354020ea9cda293d7f83
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-19 21:09:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2625,7 +2625,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         except:
             pass
             return
-        else:            
+                    
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
         settings = await get_settings(message.chat.id)
