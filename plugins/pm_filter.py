a906097d45ecfbe1cde882cc131f8f7bd9dbb635
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-19 16:37:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2601,7 +2601,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
             search = search.replace("-", " ")
             search = search.replace(":", "")
             search = search.replace(".", "")
-            files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
+            files, offset, total_results = await get_search_results(chat_id=message.chat.id, query=name.lower(), offset=0, filter=True)
             settings = await get_settings(message.chat.id)
     if not files:
         await reply_msg.edit_text(
