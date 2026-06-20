03fad64b674725ad2ae8e61aa934c18b9c6058cc
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-20 18:16:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,6 +27,9 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002037836704'))
+MOVIE_UPDATE_CHANNEL = int(
+    os.environ.get("MOVIE_UPDATE_CHANNEL", "-1001234567890")
+)
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002357544388').split()]  # For Multiple Id Use One Space Between Each.
