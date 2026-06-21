325393f72a7050fb8c91699cc99ab14a6275e99f
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-21 10:34:43 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -41,7 +41,7 @@ async def media(bot, message):
         search = re.sub(r'\bHDTS\b', '', search, flags=re.IGNORECASE)
         search = re.sub(r'\bEnglish\b', '', search, flags=re.IGNORECASE)
         search = re.sub(r'\bx264\b', '', search, flags=re.IGNORECASE)
-        search = re.sub(r'\b(Hindi|English|Tamil|Telugu|Malayalam|Kannada|Punjabi|Bengali|Marathi|Gujarati|Dual|Multi)\b', '', search, flags=re.IGNORECASE)
+        search = re.sub(r'\b(Hindi|English|Tamil|Telugu|Malayalam|French|Kannada|Punjabi|Bengali|Marathi|Gujarati|Dual|Multi)\b', '', search, flags=re.IGNORECASE)
         search = re.sub(r'\bESubs\b', '', search, flags=re.IGNORECASE)
         search = re.sub(r'@[^ ]+', '', search)
         search = re.sub(r'[^a-zA-Z0-9 ]', ' ', search)
