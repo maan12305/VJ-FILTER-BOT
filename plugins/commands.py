a80394048c92171e4c959e24ec231028f997418f
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-21 14:15:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -516,16 +516,15 @@ async def start(client, message):
             await k.edit("<b>✅ ʏᴏᴜʀ ᴍᴇssᴀɢᴇ ɪs sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ</b>")
             return
     user = message.from_user.id
-    files_ = await get_file_details(file_id)           
-          if not files_:
-             if data.startswith("search_"):
-                movie = data.replace("search_", "").replace("_", " ")
-                ai_search = True
-                reply_msg = await message.reply_text(f"<b><i>Searching For {movie} 🔍</i></b>")
-                message.text = movie
-                await auto_filter(client, movie, message, reply_msg, ai_search)
-                return
-
+    files_ = await get_file_details(file_id)
+    if not files_:
+        if data.startswith("search_"):
+            movie = data.replace("search_", "").replace("_", " ")
+            ai_search = True
+            reply_msg = await message.reply_text(f"<b><i>Searching For {movie} 🔍</i></b>")
+            message.text = movie
+            await auto_filter(client, movie, message, reply_msg, ai_search)
+            return
             
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
