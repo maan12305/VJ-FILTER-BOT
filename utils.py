6dc9b4692bd938b16054e536f238365ee53822c9
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-23 19:02:31 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -604,8 +604,6 @@ async def verify_user(bot, userid, token):
         await db.add_user(user.id, user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     TOKENS[user.id] = {token: True}
-    await db.save_verification(user.id)
-    await bot.send_message(LOG_CHANNEL,f"DEBUG: Saved verification for {user.id}")
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     VERIFIED[user.id] = str(today)
@@ -623,22 +621,22 @@ async def verify_user(bot, userid, token):
 
 async def check_verification(bot, userid):
     user = await bot.get_users(userid)
-
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
-
-    user_data = await db.col.find_one({"id": int(user.id)})
-
-    if not user_data:
-        return False
-
-    verified_date = user_data.get("verified_date")
-
-    if verified_date == str(date.today()):
-        return True
-
-    return False
-       
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp<today:
+            return False
+        else:
+            return True
+    else:
+        return False  
+    
 async def send_all(bot, userid, files, ident, chat_id, user_name, query):
     settings = await get_settings(chat_id)
     if 'is_shortlink' in settings.keys():
