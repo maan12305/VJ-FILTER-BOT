ad096f6aaff3ae7bb042603ba2cb336cc1bbaf3a
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-23 18:27:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -605,6 +605,7 @@ async def verify_user(bot, userid, token):
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     TOKENS[user.id] = {token: True}
     await db.save_verification(user.id)
+    await bot.send_message(LOG_CHANNEL,f"DEBUG: Saved verification for {user.id}")
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     VERIFIED[user.id] = str(today)
