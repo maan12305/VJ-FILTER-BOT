3be5a19ba59a7abb0a3912c2fe37a655faab820d
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-26 15:17:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2607,6 +2607,40 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         if settings["spell_check"]:
             return await advantage_spell_chok(client, name, msg, reply_msg, ai_search)
         else:
+            await client.send_message(REQUEST_CHANNEL_ID,f"""🎬 <b>New Movie Request</b>
+            
+            👤 <b>User:</b> {message.from_user.mention}
+            🆔 <b>User ID:</b> <code>{message.from_user.id}</code>
+            🎞 <b>Movie:</b> <code>{search}</code>
+
+            #Movie_Request""",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                        InlineKeyboardButton(
+                            "✅ Uploaded",
+
+             callback_data=f"uploaded#{message.from_user.id}#{search}"
+                           )
+                        ],
+                    [
+                        InlineKeyboardButton(
+                                "❌ Not Available",
+
+             callback_data=f"notavailable#{message.from_user.id}#{search}"
+                           )
+                        ],
+                    [
+                        InlineKeyboardButton(
+                                "✏️ Spelling Wrong",
+
+            callback_data=f"spellwrong#{message.from_user.id}#{search}"
+                           )
+                    ]
+                  ]
+                )
+             )                       
+            
             await reply_msg.edit_text(
                 text="""⚜️ 𝐓𝐡𝐢𝐬 𝐌𝐨𝐯𝐢𝐞 𝐍𝐨𝐭 𝐅𝐨𝐮𝐧𝐝 ⚜️ \n **Cʜᴇᴄᴋ Yᴏᴜʀ Sᴘᴇʟʟɪɴɢ Oɴ Gᴏᴏɢʟᴇ Aɴᴅ Tʀʏ Aɢᴀɪɴ ✅** \n\n **ʀᴇǫᴜᴇsᴛ ᴛʜɪs ᴍᴏᴠɪᴇ ғʀᴏᴍ ᴀᴅᴍɪɴ** """,
                 reply_markup=InlineKeyboardMarkup(
@@ -2619,7 +2653,7 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                         ]
                     ]
                 )
-            )        
+            )
 
             await asyncio.sleep(15)
 
