692c036c3a77b7b7501d87de6adfea37007bb088
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-06-27 10:41:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1122,6 +1122,7 @@ async def filter_qualities_cb_handler(client: Client, query: CallbackQuery):
                 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
+    print(query.data)
     if query.data == "close_data":
         await query.message.delete()
 
