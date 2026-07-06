7af2658653d49eac16a42754e57b74c7aaf6dc6e
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-07-06 13:13:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ from pyrogram import Client, idle
 from database.users_chats_db import db
 from info import *
 from utils import temp
+from plugins.premium_reminder import premium_reminder
 from typing import Union, Optional, AsyncGenerator
 from Script import script 
 from datetime import date, datetime 
@@ -40,6 +41,9 @@ async def start():
     await db.verify.create_index("expiry_time",expireAfterSeconds=0)
     bot_info = await TechVJBot.get_me()
     await initialize_clients()
+    
+    asyncio.create_task(premium_reminder())
+
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
