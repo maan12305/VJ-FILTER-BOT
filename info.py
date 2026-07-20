aa6b82dffe08f5fc89f3c88072780c095f01ab27
maan12305 <167197439+maan12305@users.noreply.github.com>
2026-07-20 10:20:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -94,7 +94,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/maan_movies2')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movie_update_channels')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/backup_group1300') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Im_professor_22')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Chat_With_Proffessor_bot')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
